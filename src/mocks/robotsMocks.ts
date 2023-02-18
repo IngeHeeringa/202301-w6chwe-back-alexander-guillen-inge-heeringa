6d9c0ff39cb24f03c37fd9d8f7ec86ac78fe48c3
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-18 20:45:14 +0100

    Add header to getRobots for Access-Control-Allow-Origin

--- src/mocks/robotsMocks.ts
+++ src/mocks/robotsMocks.ts
@@ -4,6 +4,6 @@ export const requestMock = {} as Request;
 export const responseMock = {
   status: jest.fn().mockReturnThis(),
   json: jest.fn(),
-  set: jest.fn(),
+  header: jest.fn(),
 } as Partial<Response>;
 export const nextMock = jest.fn() as NextFunction;
