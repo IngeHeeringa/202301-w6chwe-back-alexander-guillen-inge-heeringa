d3f26979ceb9c8d2cee320d67352d6b82da2d2f7
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-18 20:07:50 +0100

    Add Access-Control-Allow-Origin permissions

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import morgan from "morgan";
 import robotsRouter from "./routers/robotsRouter.js";
+
 const app = express();
+
+app.disable("x-powered-by");
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+});
 
 app.use("/", robotsRouter);
 
