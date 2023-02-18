a544c6c0d2ad8c4287c5bfc17fd6b133dcce676b
Alexander <aguillenhernandez95@gmail.com>
2023-02-18 09:08:42 +0100

    Add notFoundError and generalError middlewares

---
+++ src/server/middlewares/errorMiddlewares.ts
@@ -0,0 +1,23 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { CustomError } from "../../CustomError/CustomError";
+
+export const notFoundError = (req: Request, res: Response) => {
+  const { statusCode, publicMessage } = new CustomError(
+    "Path not found",
+    404,
+    "Endpoint not found"
+  );
+
+  res.status(statusCode).json({ error: publicMessage });
+};
+
+export const generalError = (
+  { statusCode, publicMessage }: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res
+    .status(statusCode || 500)
+    .json({ error: publicMessage || "Endpoint not found" });
+};
