505131296a3a0712f308fb0ec01718c6789fef5f
Alexander <aguillenhernandez95@gmail.com>
2023-02-18 14:16:41 +0100

    Refactor notFoundError to only include nexting functionality

--- src/server/middlewares/errorMiddlewares.test.ts
+++ src/server/middlewares/errorMiddlewares.test.ts
@@ -1,43 +1,22 @@
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { CustomError } from "../../CustomError/CustomError";
-import { generalError, notFoundError } from "./errorMiddlewares";
-
-const request = {} as Request;
-
-const response = {
-  status: jest.fn().mockReturnThis(),
-  json: jest.fn(),
-} as Partial<Response>;
+import { notFoundError } from "./errorMiddlewares";
 
 describe("Given a notFoundError middleware", () => {
-  describe("When it receives a response and a request", () => {
-    test("Then it should emmit a response with status code 404 and an error message 'Endpoint not found'.", () => {
-      notFoundError(request, response as Response);
-
-      expect(response.status).toHaveBeenCalledWith(404);
-
-      expect(response.json).toHaveBeenCalledWith({
-        error: "Endpoint not found",
-      });
-    });
-  });
-});
-
-describe("Given a generalError middleware", () => {
-  describe("When it receives a response, a request and an error that has status code 500 and public message 'Endpoint not found'", () => {
-    test("Then it should emmit a response with status 500 and an error message 'Endpoint not found'", () => {
-      const error = new CustomError(
+  describe("When it receives a response, resquest and next function", () => {
+    test("Then it should next a not found error", () => {
+      const request = {} as Request;
+      const response = {} as Response;
+      const next = jest.fn() as NextFunction;
+      const expectedNextedError = new CustomError(
         "Path not found",
         500,
         "Endpoint not found"
       );
 
-      generalError(error, request, response as Response);
+      notFoundError(request, response, next);
 
-      expect(response.status).toHaveBeenCalledWith(500);
-      expect(response.json).toHaveBeenCalledWith({
-        error: "Endpoint not found",
-      });
+      expect(next).toHaveBeenCalledWith(expectedNextedError);
     });
   });
 });
--- src/server/middlewares/errorMiddlewares.ts
+++ src/server/middlewares/errorMiddlewares.ts
@@ -1,14 +1,18 @@
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { CustomError } from "../../CustomError/CustomError";
 
-export const notFoundError = (req: Request, res: Response) => {
-  const { statusCode, publicMessage } = new CustomError(
+export const notFoundError = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const notFoundError = new CustomError(
     "Path not found",
     404,
     "Endpoint not found"
   );
 
-  res.status(statusCode).json({ error: publicMessage });
+  next(notFoundError);
 };
 
 export const generalError = (
