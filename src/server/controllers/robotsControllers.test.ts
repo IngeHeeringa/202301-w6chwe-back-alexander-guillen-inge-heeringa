41068121a59ac36fb41bd11ccfd5692d458e87ec
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-20 23:13:14 +0100

    Add tests robotControllers error handling

--- src/server/controllers/robotsControllers.test.ts
+++ src/server/controllers/robotsControllers.test.ts
@@ -13,8 +13,8 @@ import {
 } from "./robotsControllers.js";
 
 describe("Given a getRobots controller", () => {
-  describe("When it receives a response and Robot.find returns and empty object", () => {
-    test("Then it should call status code 200 and json methods from that response", async () => {
+  describe("When it receives a response and Robot.find returns a collection of robots", () => {
+    test("Then it should call the status method with code 200 from that response", async () => {
       Robot.find = jest.fn().mockImplementationOnce(() => ({
         exec: jest.fn().mockReturnValue({}),
       }));
@@ -22,12 +22,23 @@ describe("Given a getRobots controller", () => {
       await getRobots(requestMock, responseMock as Response, nextMock);
 
       expect(responseMock.status).toHaveBeenCalledWith(200);
+    });
+  });
+
+  describe("When it receives a response and Robot.find returns a collection of robots", () => {
+    test("Then it should call the json method from that response", async () => {
+      Robot.find = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockReturnValue({}),
+      }));
+
+      await getRobots(requestMock, responseMock as Response, nextMock);
+
       expect(responseMock.json).toHaveBeenCalled();
     });
   });
 
   describe("When it receives a response and Robot.find returns an error", () => {
-    test("Then it should call next function with getRobots error with status code 500 and public message 'Sorry, we could not retrieve robots'", async () => {
+    test("Then it should call next function with getRobots error with status code 500", async () => {
       Robot.find = jest.fn().mockReturnValue(new Error());
 
       await getRobots(requestMock, responseMock as Response, nextMock);
@@ -35,7 +46,6 @@ describe("Given a getRobots controller", () => {
       expect(nextMock).toHaveBeenCalledWith(
         expect.objectContaining({
           statusCode: 500,
-          publicMessage: "Sorry, we could not retrieve robots",
         })
       );
     });
@@ -43,8 +53,8 @@ describe("Given a getRobots controller", () => {
 });
 
 describe("Given a getRobotById controller", () => {
-  describe("When it receives a request and response", () => {
-    test("Then it should emit a response with status code 200 and call its json method", async () => {
+  describe("When it receives a response andRobot.findById returns a robot", () => {
+    test("Then it should call the status method with code 200", async () => {
       Robot.findById = jest.fn().mockImplementationOnce(() => ({
         exec: jest.fn().mockReturnValue({}),
       }));
@@ -52,14 +62,39 @@ describe("Given a getRobotById controller", () => {
       await getRobotById(requestMock, responseMock as Response, nextMock);
 
       expect(responseMock.status).toHaveBeenCalledWith(200);
+    });
+  });
+
+  describe("When it receives a response andRobot.findById returns a robot", () => {
+    test("Then it should call the json method from that response", async () => {
+      Robot.findById = jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockReturnValue({}),
+      }));
+
+      await getRobotById(requestMock, responseMock as Response, nextMock);
+
       expect(responseMock.json).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives a response and Robot.findById returns an error", () => {
+    test("Then it should call next function with getRobotById error with status code 500", async () => {
+      Robot.findById = jest.fn().mockReturnValue(new Error());
+
+      await getRobotById(requestMock, responseMock as Response, nextMock);
+
+      expect(nextMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          statusCode: 500,
+        })
+      );
     });
   });
 });
 
 describe("Given a deleteRobotById controller", () => {
-  describe("When it receives a request and response", () => {
-    test("Then it should emit a response with status code 200 and call its json method", async () => {
+  describe("When it receives a response and Robot.findByIdAndDelete returns the deleted robot", () => {
+    test("Then it should call the status method with code 200", async () => {
       Robot.findByIdAndDelete = jest.fn().mockImplementationOnce(() => ({
         exec: jest.fn().mockReturnValue({}),
       }));
@@ -67,6 +102,15 @@ describe("Given a deleteRobotById controller", () => {
       await deleteRobotById(requestMock, responseMock as Response, nextMock);
 
       expect(responseMock.status).toHaveBeenCalledWith(200);
+    });
+  });
+
+  describe("When it receives a response and Robot.findByIdAndDelete returns an error", () => {
+    test("Then it should call next function with deleteRobotById error with status code 500", async () => {
+      Robot.findByIdAndDelete = jest.fn().mockReturnValue(new Error());
+
+      await deleteRobotById(requestMock, responseMock as Response, nextMock);
+
       expect(responseMock.json).toHaveBeenCalled();
     });
   });
